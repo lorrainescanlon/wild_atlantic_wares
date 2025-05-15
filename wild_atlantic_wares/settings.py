61379b0616855fbde7f89d73826c90fbcf333691
lorrainescanlon <lorrainescanlon@gmail.com>
2025-05-15 01:02:01 +0100

    Update settings.py debug to false

--- wild_atlantic_wares/settings.py
+++ wild_atlantic_wares/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1+6l#rx63unqmop2ja@vnab9fvze!*72e*fhr)39koojy_u$vo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/',]
 
 # Application definition
 
