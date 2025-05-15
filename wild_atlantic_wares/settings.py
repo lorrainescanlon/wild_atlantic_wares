9929687d94e5cfa694bdfb6be3455f986436902a
lorrainescanlon <lorrainescanlon@gmail.com>
2025-05-15 23:07:31 +0100

    Updated header layout, moved delivery banner above navbar. Reduced serach box size. Updated header and footer spacing. Added footer content, internal links, social media links, comtact and subscribe sections. Updated font embed code in css updated font css classes.

--- wild_atlantic_wares/settings.py
+++ wild_atlantic_wares/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1+6l#rx63unqmop2ja@vnab9fvze!*72e*fhr)39koojy_u$vo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/',]
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 # Application definition
 
