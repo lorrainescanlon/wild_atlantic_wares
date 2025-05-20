56d16a68566f99669da7b93fe213febda44d57df
lorrainescanlon <lorrainescanlon@gmail.com>
2025-05-20 01:16:37 +0100

    Updated html templates with products url and added search and sort views to products views.py to enable search funtion

--- products/views.py
+++ products/views.py
@@ -1,16 +1,51 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q, Lower
 from .models import Product
-
 
 
 def all_products(request):
     """A view to return all products"""
 
     products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                  direction = request.GET['direction']
+                  if direction =='desc':
+                     sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+         if 'category' in request.GET:
+               categories = request.GET['category'].split(',')
+               products = products.filter(category__name__in=categories)
+               categories = Category.objects.filter(name__in=categories)
+                
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Search criteria can not be blank")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-       'products': products,
-    }
+        'products': products,
+        'search_term': query,
+      }
 
     return render(request, 'products/products.html', context)
 
