c9baf35b62580c3b386ffadcf6e792b6df59d111
lorrainescanlon <lorrainescanlon@gmail.com>
2025-05-31 16:46:41 +0100

    Added OrderAdmin and OrderLineItemAdminInline to checkout admin.py. Created checkout signals.py file to call the Order model update_total method each time a post_save and post_delete signal is received from the OrderLineItem model. Updated apps.py to overide the ready method and import the new signals module

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInine(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total,')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInine,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+    
+    fields = ('order_number', 'full_name', 'date',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'address1',
+              'address2', 'county', 'deivery_cost',
+              'order_total', 'grand_total',)
+    
+    list_display = ('order_number', 'full_name', 'date',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
