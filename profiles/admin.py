495023b035cfa8c6fbf0fe37b194fa7a6ab34d17
lorrainescanlon <lorrainescanlon@gmail.com>
2025-06-05 00:45:45 +0100

    Created profile form, updated view and template to render form and pull any orders associated with a user profile. Updated success toasts so that they do not display the shopping cart as part of the message while on the user profile page. Updated profile admin.py. Updated form placeholder color in profile.css.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile
+
 
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ( 'user',
+                     'default_country',
+                     'default_county',
+                     'default_town_or_city',
+    )
+
+    ordering = ('user',)
+
+
+
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
---
+++ profiles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',) 
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders remove lables and autofocus
+        on default_phone_number field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Telephone',
+            'default_address1': 'Street Address 1',
+            'default_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County',
+            'default_postcode': 'Postcode',
+            'default_country': 'Country',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black profile-form-input'
+            self.fields[field].label = False
